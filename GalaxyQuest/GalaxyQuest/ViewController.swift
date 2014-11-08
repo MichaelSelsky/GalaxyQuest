615c7561264537efc892116502dc1c8330d931fc
Michael Selsky <michael.selsky@gmail.com>
2014-11-08 13:30:53 -0500

    Created xcodeproject

---
+++ GalaxyQuest/GalaxyQuest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalaxyQuest
+//
+//  Created by Michael Selsky on 11/8/14.
+//  Copyright (c) 2014 Michael Selsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
