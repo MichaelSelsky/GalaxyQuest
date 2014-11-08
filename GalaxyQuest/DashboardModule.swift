580365a917194f770262fb003129518f7a28a6e3
Michael Selsky <michael.selsky@gmail.com>
2014-11-08 16:13:50 -0500

    Created a base model for the control

--- GalaxyQuest/DashboardModule.swift
+++ GalaxyQuest/DashboardModule.swift
@@ -6,8 +6,27 @@
 //  Copyright (c) 2014 Michael Selsky. All rights reserved.
 //
 
+/*
+ * Individual control that will be part of all the dashboard controls for the spaceship
+ */
+
 import UIKit
 
+enum  DashboardControlType{
+    case Switch
+    case Slider
+    case Button
+}
+
 class DashboardModule: NSObject {
-   
+    var type: DashboardControlType
+    var name: String
+    var value: Int
+    
+    init(type: DashboardControlType, name: String){
+        self.type = type
+        self.name = name
+        self.value = 0
+        super.init()
+    }
 }
