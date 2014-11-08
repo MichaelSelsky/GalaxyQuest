89780e59b46a165764c6688e11e9be7091ecd96e
Michael Selsky <michael.selsky@gmail.com>
2014-11-08 16:11:19 -0500

    Added a button to start advertising and browsing for peers

---
+++ GalaxyQuest/DashboardModule.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardModule.swift
+//  GalaxyQuest
+//
+//  Created by Michael Selsky on 11/8/14.
+//  Copyright (c) 2014 Michael Selsky. All rights reserved.
+//
+
+import UIKit
+
+class DashboardModule: NSObject {
+   
+}
--- GalaxyQuest/GalaxyQuest/ViewController.swift
+++ GalaxyQuest/GalaxyQuest/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let session = ConnectivityManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func advertise(sender: AnyObject) {
+        self.session.beginAdvertising()
+        self.session.beginBrowsing()
+    }
 
 }
 
