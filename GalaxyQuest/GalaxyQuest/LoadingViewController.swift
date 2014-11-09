1075ed934d5b3abb3fe2448384d67c35084569a7
Michael Selsky <michael.selsky@gmail.com>
2014-11-08 21:10:26 -0500

    Beginning UI

---
+++ GalaxyQuest/GalaxyQuest/LoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadingViewController.swift
+//  GalaxyQuest
+//
+//  Created by Michael Selsky on 11/8/14.
+//  Copyright (c) 2014 Michael Selsky. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
